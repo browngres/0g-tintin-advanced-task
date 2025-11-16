cbb0ed6e6b2b525d92a45162e0d71883a15ff2d0
Rainy <yusi_rainy@outlook.com>
2025-11-16 14:38:41 +0800

    本来打算直接给参数，后面发现还是要构建 uploadArgs

--- Task2/main.go
+++ Task2/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/rand"
 	"fmt"
+	"math"
 	"math/big"
 	"os"
 
@@ -29,16 +30,17 @@ const (
 
 // 上传参数
 var (
-	fee               *big.Int = big.NewInt(10000000000000000)
-	task_size                  = 10
-	finality_required          = transfer.FileFinalized
-	expected_replica           = 1
-	skip_tx                    = true
-	num_retries                = 3
-	step              int64    = 15
-	method                     = "min"
-	full_trusted               = true
-	timeout                    = time.Duration(300) * time.Second
+	fee                     = 0.01
+	task_size               = 10
+	finality_required       = transfer.FileFinalized
+	expected_replica        = 1
+	skip_tx                 = true
+	num_retries             = 3
+	step              int64 = 15
+	method                  = "min"
+	full_trusted            = true
+	timeout                 = time.Duration(300) * time.Second
+	go_routines             = runtime.GOMAXPROCS(0)
 )
 
 // 生成指定大小文件
@@ -60,6 +62,40 @@ func generateFile(size uint64, file_name string) (string, error) {
 // 删除指定文件
 func deleteFile(file_name string) {
 
+}
+
+// 参数相关结构体
+type transactionArgument struct {
+	url   string
+	key   string
+	fee   float64
+	nonce uint
+}
+
+type uploadArgument struct {
+	transactionArgument
+
+	file string
+	tags string
+
+	node    []string
+	indexer string
+
+	expectedReplica uint
+
+	skipTx           bool
+	finalityRequired bool
+	taskSize         uint
+	routines         int
+
+	fragmentSize int64
+	maxGasPrice  uint
+	nRetries     int
+	step         int64
+	method       string
+	fullTrusted  bool
+
+	timeout time.Duration
 }
 
 type UploadOption struct {
@@ -84,8 +120,6 @@ func main() {
 	ctx, cancel = context.WithTimeout(ctx, timeout)
 	defer cancel()
 	_ = ctx
-
-	time.Sleep(time.Duration(6) * time.Second)
 
 	// 读取环境变量
 	if err := godotenv.Load("../.env"); err != nil {
@@ -115,8 +149,6 @@ func main() {
 		return
 	}
 
-	// TODO go_routines = runtime.GOMAXPROCS(0)
-
 	// 上传
 	/*
 		构建一个 `transfer.UploadOption` ，然后把文件读到内存里面。
@@ -124,19 +156,48 @@ func main() {
 		最终获取到 root，单个文件最大是 4G，如果超过 4G 会有多个 root。
 	*/
 
+	// 构建 uploadArgs
+	uploadArgs := uploadArgument{
+		transactionArgument: transactionArgument{
+			url:   TESTNET_RPC,
+			key:   PRIVATE_KEY,
+			fee:   fee,
+			nonce: 0,
+		},
+		file:             temp_file,
+		tags:             "0x",
+		node:             []string{},
+		indexer:          TESTNET_INDEXER,
+		expectedReplica:  uint(expected_replica),
+		skipTx:           skip_tx,
+		finalityRequired: true,
+		taskSize:         uint(task_size),
+		routines:         go_routines,
+		fragmentSize:     FRAGMENT_SIZE,
+		maxGasPrice:      uint(0),
+		nRetries:         num_retries,
+		step:             step,
+		method:           method,
+		fullTrusted:      full_trusted,
+		timeout:          timeout,
+	}
+
+	// 构建 UploadOption
+	fee_wei, _ := new(big.Float).Mul(new(big.Float).SetFloat64(uploadArgs.fee), big.NewFloat(math.Pow10(18))).Int(nil)
+
 	opt := UploadOption{
-		Tags:             []byte{},
-		FinalityRequired: finality_required,
-		TaskSize:         uint(task_size),
-		ExpectedReplica:  uint(expected_replica),
-		SkipTx:           skip_tx,
-		Fee:              fee,
+		Tags:             []byte{},          // "0x" 解析后是空字符串
+		FinalityRequired: finality_required, // 没有用 uploadArgs
+		TaskSize:         uploadArgs.taskSize,
+		ExpectedReplica:  uploadArgs.expectedReplica,
+		SkipTx:           uploadArgs.skipTx,
+		Fee:              fee_wei,
 		Nonce:            big.NewInt(0),
 		MaxGasPrice:      big.NewInt(0),
-		NRetries:         num_retries,
-		Step:             step,
-		Method:           method,
-		FullTrusted:      full_trusted,
+		NRetries:         uploadArgs.nRetries,
+		Step:             uploadArgs.step,
+		Method:           uploadArgs.method,
+		FullTrusted:      uploadArgs.fullTrusted,
 	}
 	_ = opt
 	// fmt.Println(opt)
@@ -147,6 +208,8 @@ func main() {
 		fmt.Println("Failed to open file")
 	}
 	defer file.Close()
+
+	// 实例化 uploader
 
 	// 下载
 
